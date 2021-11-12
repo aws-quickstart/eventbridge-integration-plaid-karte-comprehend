146ef0a2a87f36c2d82fd10c59b719867ad2108f
ikemonn <daiki.matsui@plaid.co.jp>
2021-11-12 14:12:41 +0900

    remove unused code

--- src/GetSentiment/getSentiment.js
+++ src/GetSentiment/getSentiment.js
@@ -27,56 +27,58 @@ async function trackEvents(keys, events) {
 exports.handler = async (event, context) => {
   const content = JSON.parse(event.Records[0].Sns.Message).detail.content;
   const {answered_question, visitor_id} = content;
-  let {languageCode} = content;
-  // console.log("content from sns", content);
+  let { languageCode } = content;
 
   if (answered_question == null || visitor_id == null) {
-    const err = new Error(`Missing required field`)
-    console.log(err)
+    const err = new Error(`Missing required field`);
+    console.log(err);
     return err;
   }
 
-  if (languageCode == null) languageCode = 'ja';
+  if (languageCode == null) languageCode = "ja";
 
   let sentiment, sentimentScore, keyPhrases;
 
   try {
     const [sentimentData, keyPhraseData] = await Promise.all([
-      comprehend.detectSentiment({
-        LanguageCode: languageCode,
-        Text: answered_question
-      }).promise(),
+      comprehend
+        .detectSentiment({
+          LanguageCode: languageCode,
+          Text: answered_question,
+        })
+        .promise(),
 
-      comprehend.detectKeyPhrases({
-        LanguageCode: languageCode,
-        Text: answered_question
-      }).promise()
+      comprehend
+        .detectKeyPhrases({
+          LanguageCode: languageCode,
+          Text: answered_question,
+        })
+        .promise(),
     ]);
 
-    sentiment = sentimentData.Sentiment
+    sentiment = sentimentData.Sentiment;
     sentimentScore = sentimentData.SentimentScore;
     keyPhrases = keyPhraseData.KeyPhrases;
-
   } catch (err) {
     console.log(err);
     return err;
   }
 
-
   const keys = {
-    user_id: visitor_id
-  };  // イベントを発生させるユーザーとの紐付け情報
+    user_id: visitor_id,
+  };
 
-  const events = [{
-    event_name: 'questionnaire_analysis',
-    values: {
-      sentiment,
-      sentimentScore,
-      keyPhrases
-    }
-  }];
+  const events = [
+    {
+      event_name: "questionnaire_analysis",
+      values: {
+        sentiment,
+        sentimentScore,
+        keyPhrases,
+      },
+    },
+  ];
 
-  // console.log(`start to trackEvents ${JSON.stringify(keys, null, 2)} ${JSON.stringify(events, null, 2)}`)
   try {
     const res = await trackEvents(keys, events);
     if (res.status !== 200) {
